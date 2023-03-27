b84959f515877f36f20a9a03f119c90d0252457e
yakkantiChakradhar <120570592+yakkantiChakradhar@users.noreply.github.com>
2023-03-27 11:27:45 +0530

    new

---
+++ src/newly/ConditionalRendering_8.js
@@ -0,0 +1,61 @@
+/* 
+Conditional Rendering :-
+1> if/else
+2> Element Variable
+3> Ternary conditional operator
+4> Short circuit operator
+*/
+
+//  1> If/else conditional rendering
+
+import React, { Component } from 'react'
+
+class ConditionalRendering extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         isLoggedIn:true
+      }
+    }
+    
+  render() {
+
+
+    /*1>  If/else conditional rendering: - 
+
+    Conditional rendering if/else statement is used inside the condition jsx is used inside the conditional rendering    
+    if(this.state.isLoggedIn){
+        return (
+            <div>Welcome Vishwas</div>
+        )
+    }else {
+        return(
+            <div>Welcome siva</div>
+        )
+    } */
+
+
+    /* 
+    2> Element variable
+    
+    let message
+    if(this.state.isLoggedIn){
+        message = <div>Welcome shiva</div>
+    }else{
+        message = <div> welcome daniel</div>
+    }
+
+    return <div>{message}</div> */
+
+   /* 3> Ternary conditional operator
+
+   return (
+        this.state.isLoggedIn ? <div>Welcome shiva</div> : <div>welcome daniel</div>
+    ) */
+/* 4> short circuit*/
+    return this.state.isLoggedIn && <div>Welcome vishwas</div>
+
+  }
+}
+export default ConditionalRendering
---
+++ src/newly/ListRendering_9.1.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ListRenderingChild from './ListRenderingChild';
+
+function ListRender() {
+    const persons = [{id:1,name:"charan",age:23,skill:"React"},
+                     {id:2,name:"mahathma",age:25,skill:"VUE"},
+                     {id:3,name:"simran",age:24,skill:"Angular"},
+                     {id:4,name:"king",age:21,skill:"Text"}];
+    const personList = persons.map((person)=><ListRenderingChild key={person.id} person={person} />)              
+  return (
+    <div>{personList}</div>
+  )
+}
+
+export default ListRender
+
+/* 
+Lists and keys summary: -
+1> A "key" is a special string attribute you need to include when creating lists of elements.
+
+2> Keys give the elements a stable identity.
+
+3> keys help REACT identity which items have changed, are added, or are removed.
+
+4> Help in efficient update of the user interface.
+
+*/
