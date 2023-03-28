3fe7f8a4745313e867394deeab8303beb565649c
yakkantiChakradhar <120570592+yakkantiChakradhar@users.noreply.github.com>
2023-03-28 08:48:03 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import PropsOfReact from './newly/PropsOfReact_1';
+/* import PropsOfReact from './newly/PropsOfReact_1';
 import StateOfReact from './newly/StateOfReact_2';
 import SetStateOfReact from './newly/SetStateOfReact_3';
 import DestucturingInReact from './newly/DestucturingInReact_4';
@@ -7,12 +7,15 @@ import EventHandling from './newly/EventHandling_5';
 import EventBinding from './newly/EventBinding_6';
 import MethodsAsProps from './newly/MethodsAsProps_7';
 import ConditionalRendering from './newly/ConditionalRendering_8';
-/* import ListRendering from './newly/ListRendering_9'; */
-import ListRender from './newly/ListRendering_9.1';
+ import ListRendering from './newly/ListRendering_9'; 
+import ListRender from './newly/ListRendering_9.1'; */
+import StyleSheets from './newly/StyleSheets';
+import './cssStyle.css';
+import styles from './cSSStyle.module.css'
 function App() {
   return (
     <div className="App">
-    <PropsOfReact name="cran" atName="crayon" />
+    {/* <PropsOfReact name="cran" atName="crayon" />
     <StateOfReact />
     <SetStateOfReact /><br />
     <DestucturingInReact  name="abhinav" firstName="chandra paul"/>
@@ -22,8 +25,10 @@ function App() {
     
     <ConditionalRendering />
     {/* <ListRendering /> */}
-    <ListRender />
-
+   {/*  <ListRender />  */}
+<StyleSheets primary={true} />
+<h1 className='error'>Error handling</h1>
+<h2 className={styles.handling}>Editing handiled</h2>
     </div>
   );
 }
