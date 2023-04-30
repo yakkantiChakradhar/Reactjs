b47982f9a3b097138470c1adf18cf62c43e78d1c
yakkantiChakradhar <120570592+yakkantiChakradhar@users.noreply.github.com>
2023-04-30 09:46:00 +0530

    error boundary

--- src/newly/ErrorBoundary.js
+++ src/newly/ErrorBoundary.js
@@ -1,3 +1,14 @@
+/* 
+Error Boundaries: -
+````````````````
+** Error boundaries are react components that catch javascript error in their child component tree, log those errors and display a fall-back UI.
+
+** A class component becomes an error boundary by defining either or both of getDerivedStateFromError and componentDidCatch life cycle methods
+
+** The placement of errror boundaries is also matter as it contains if the entire app should have the fall-back UI or just the component causing the problem
+
+** Provide a way to gracefully handle error in application code.
+*/
 import React, { Component } from 'react'
 
 class ErrorBoundary extends Component {
@@ -27,3 +38,5 @@ return this.props.children
 }
 
 export default ErrorBoundary
+
+
